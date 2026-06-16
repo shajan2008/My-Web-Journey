fffd2d42daa052dfe1787ba84080ab7f32883765
shajans <shajan072008@gmail.com>
2026-06-16 18:13:02 +0530

    Website aktualisiert

--- Front end/JavaScript/Interaction.js
+++ Front end/JavaScript/Interaction.js
@@ -1,9 +1,20 @@
 let toggle = document.getElementById(`togglebtn`);
+let icon = document.querySelector(`.icon`);
 toggle.addEventListener(`change`, function(){
     if(toggle.checked){
         document.body.style.backgroundColor = `white`;
+        document.body.style.color = `black`;
+        icon.textContent = `⭐️`;
+        document.querySelectorAll(`td, th`).forEach(cell => {
+            cell.style.color = `#000000`;
+        })
     }
     else{
-        document.body.style.backgroundColor = `#0A0A0A`;
+        document.body.style.backgroundColor = `black`;
+        document.body.style.color =`#ffffff`;
+        icon.textContent = `☀️`;
+        document.querySelectorAll(`td, th`).forEach(cell => {
+            cell.style.color = `#ffffff`;
+        });
     }
 })
